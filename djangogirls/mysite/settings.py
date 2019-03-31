1da64b283e3a0139c3672f03b3aa0365cbd1a9e2
Masaaki Uno <unokun@gmail.com>
2019-03-31 17:45:31 +0900

    完成

--- djangogirls/mysite/settings.py
+++ djangogirls/mysite/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=(^ym9g%%6vqyyu#_i$3h_#mj%18&i)bdv3w5hhfk@8)8v0y!&'
+SECRET_KEY = 'zyo3_6x)d&zwklj5kot-bx^z-ys35hwl=enchx7i^vf@+&dkx-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
